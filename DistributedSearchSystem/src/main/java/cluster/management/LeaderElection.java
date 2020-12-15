dace53bd5b2eec4da2711b3ba309c36ae78f46f1
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2020-12-15 18:12:00 -0500

    relect leader method added

--- DistributedSearchSystem/src/main/java/cluster/management/LeaderElection.java
+++ DistributedSearchSystem/src/main/java/cluster/management/LeaderElection.java
@@ -1,6 +1,10 @@
 package cluster.management;
 
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.Collections;
+import java.util.List;
 
 public class LeaderElection implements Watcher {
     private static final String ELECTION_NAMESPACE = "/election";
@@ -16,6 +20,27 @@ public class LeaderElection implements Watcher {
         String zNodeFullPath = this.zooKeeper.create(zNodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
         System.out.println("znode name : " + zNodeFullPath);
         this.currentZNodeName = zNodeFullPath.replace(ELECTION_NAMESPACE+"/" , "");
+    }
+
+    public void relectLeader() throws KeeperException, InterruptedException {
+        Stat predecessorStat = null;
+        String predecessorZnodeName = "";
+        while(predecessorStat == null) {
+            List<String> children = this.zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+            Collections.sort(children);
+            String smallestChild = children.get(0);
+            if (this.currentZNodeName == smallestChild) {
+                System.out.println("I am the leader");
+                return;
+            } else {
+                System.out.println("I am not the leader");
+                int predecessorIndex = Collections.binarySearch(children, currentZNodeName) -1;
+                predecessorStat = zooKeeper.exists(children.get(predecessorIndex), this);
+            }
+        }
+        System.out.println("Watching znode " + predecessorZnodeName);
+        System.out.println();
+
     }
     @Override
     public void process(WatchedEvent watchedEvent) {
