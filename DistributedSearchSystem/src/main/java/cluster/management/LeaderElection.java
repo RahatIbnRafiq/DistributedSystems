3f36ff015faf1311e79dde940b0992a066e29b13
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2020-12-15 18:02:44 -0500

    leader election constructor, adding volunteer for leadership method.

--- DistributedSearchSystem/src/main/java/cluster/management/LeaderElection.java
+++ DistributedSearchSystem/src/main/java/cluster/management/LeaderElection.java
@@ -1,4 +1,24 @@
 package cluster.management;
 
-public class LeaderElection {
+import org.apache.zookeeper.*;
+
+public class LeaderElection implements Watcher {
+    private static final String ELECTION_NAMESPACE = "/election";
+    private ZooKeeper zooKeeper;
+    private String currentZNodeName;
+
+    LeaderElection(ZooKeeper zookeeper) {
+        this.zooKeeper = zookeeper;
+    }
+
+    public void volunteerForLeadership() throws KeeperException, InterruptedException {
+        String zNodePrefix = ELECTION_NAMESPACE + "/c_";
+        String zNodeFullPath = this.zooKeeper.create(zNodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        System.out.println("znode name : " + zNodeFullPath);
+        this.currentZNodeName = zNodeFullPath.replace(ELECTION_NAMESPACE+"/" , "");
+    }
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
 }
