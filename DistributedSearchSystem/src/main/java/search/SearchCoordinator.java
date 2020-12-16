ced02bdc57a9f2d4b9672b9390f03723a3db8054
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2020-12-16 17:10:23 -0500

    create response method

--- DistributedSearchSystem/src/main/java/search/SearchCoordinator.java
+++ DistributedSearchSystem/src/main/java/search/SearchCoordinator.java
@@ -1,11 +1,24 @@
 package search;
 
+import cluster.management.ServiceRegistry;
 import com.google.protobuf.InvalidProtocolBufferException;
 import constants.Constants;
+import model.Result;
+import model.Task;
 import model.proto.SearchModel;
 import networking.OnRequestCallback;
+import org.apache.zookeeper.KeeperException;
+import utilities.TFIDF;
+
+import java.util.List;
 
 public class SearchCoordinator implements OnRequestCallback {
+    private final ServiceRegistry workersServiceRegistry;
+
+    public SearchCoordinator(ServiceRegistry workersServiceRegistry) {
+        this.workersServiceRegistry = workersServiceRegistry;
+    }
+
     @Override
     public byte[] handleRequest(byte[] requestPayload) {
         SearchModel.Request request = null;
@@ -18,6 +31,25 @@ public class SearchCoordinator implements OnRequestCallback {
             return SearchModel.Response.getDefaultInstance().toByteArray();
         }
 
+    }
+
+    private SearchModel.Response createResponse(SearchModel.Request searchRequest) throws KeeperException, InterruptedException {
+        SearchModel.Response.Builder searchResponse = SearchModel.Response.newBuilder();
+        System.out.println("Received search query: " + searchRequest.getSearchQuery());
+
+        List<String> searchTerms = TFIDF.getWordsFromLine(searchRequest.getSearchQuery());
+        List<String> workers = workersServiceRegistry.getAllServiceAddresses();
+
+        if (workers.isEmpty()) {
+            System.out.println("No search workers currently available");
+            return searchResponse.build();
+        }
+        List<Task> tasks = createTasks(workers.size(), searchTerms);
+        List<Result> results = sendTasksToWorkers(workers, tasks);
+
+        List<SearchModel.Response.DocumentStats> sortedDocuments = aggregateResults(results, searchTerms);
+        searchResponse.addAllRelevantDocuments(sortedDocuments);
+        return searchResponse.build();
     }
 
     @Override
