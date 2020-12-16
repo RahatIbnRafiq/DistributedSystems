a38a98ef6f3479827ad4e85abe82c28cd143e85d
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2020-12-15 19:58:43 -0500

    add constructor for on election action class

--- DistributedSearchSystem/src/main/java/OnElectionAction.java
+++ DistributedSearchSystem/src/main/java/OnElectionAction.java
@@ -1,9 +1,17 @@
 import cluster.management.OnElectionCallback;
+import cluster.management.ServiceRegistry;
 
 public class OnElectionAction implements OnElectionCallback {
+    private final ServiceRegistry serviceRegistry;
+    private final int port;
+
+    public OnElectionAction(ServiceRegistry serviceRegistry, int port) {
+        this.serviceRegistry = serviceRegistry;
+        this.port = port;
+    }
+
     @Override
     public void onElectedToBeLeader() {
-
     }
 
     @Override
