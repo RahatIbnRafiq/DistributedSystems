06a05b1a2a59f7bcd654f0d1de2656d2834053cb
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2020-12-16 12:30:15 -0500

    add web client, get to jdk 11

--- DistributedSearchSystem/src/main/java/OnElectionAction.java
+++ DistributedSearchSystem/src/main/java/OnElectionAction.java
@@ -1,6 +1,8 @@
 import cluster.management.OnElectionCallback;
 import cluster.management.ServiceRegistry;
+import networking.WebServer;
 import org.apache.zookeeper.KeeperException;
+import search.SearchWorker;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -8,6 +10,7 @@ import java.net.UnknownHostException;
 public class OnElectionAction implements OnElectionCallback {
     private final ServiceRegistry serviceRegistry;
     private final int port;
+    private WebServer webServer;
 
     public OnElectionAction(ServiceRegistry serviceRegistry, int port) {
         this.serviceRegistry = serviceRegistry;
@@ -22,9 +25,14 @@ public class OnElectionAction implements OnElectionCallback {
 
     @Override
     public void onworker() {
+        SearchWorker searchWorker = new SearchWorker();
+        if (webServer == null) {
+            webServer = new WebServer(port, searchWorker);
+            webServer.startServer();
+        }
         try {
             String currentServerAddress =
-                    String.format("http://%s:%d", InetAddress.getLocalHost().getCanonicalHostName(), port);
+                    String.format("http://%s:%d%s", InetAddress.getLocalHost().getCanonicalHostName(), port, searchWorker.getEndpoint());
             this.serviceRegistry.registerToCluster(currentServerAddress);
         } catch (UnknownHostException e) {
             e.printStackTrace();
--- DistributedSearchSystem/src/main/java/search/SearchWorker.java
+++ DistributedSearchSystem/src/main/java/search/SearchWorker.java
@@ -1,10 +1,19 @@
 package search;
 
 import constants.Constants;
+import model.DocumentData;
 import model.Result;
 import model.Task;
 import networking.OnRequestCallback;
 import utilities.SerializationUtils;
+import utilities.TFIDF;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SearchWorker implements OnRequestCallback {
     @Override
@@ -12,6 +21,33 @@ public class SearchWorker implements OnRequestCallback {
         Task task = (Task) SerializationUtils.deserialize(requestPayload);
         Result result = createResult(task);
         return SerializationUtils.serialize(result);
+    }
+
+    private Result createResult(Task task) {
+        List<String> documents = task.getDocuments();
+        System.out.println(String.format("Received %d documents to process", documents.size()));
+        Result result = new Result();
+
+        for (String document : documents) {
+            List<String> words = parseWordsFromDocument(document);
+            DocumentData documentData = TFIDF.createDocumentData(words, task.getSearchTerms());
+            result.addDocumentData(document, documentData);
+        }
+        return result;
+    }
+
+    private List<String> parseWordsFromDocument(String document) {
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(document);
+        } catch (FileNotFoundException e) {
+            return Collections.emptyList();
+        }
+
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        List<String> lines = bufferedReader.lines().collect(Collectors.toList());
+        List<String> words = TFIDF.getWordsFromDocument(lines);
+        return words;
     }
 
     @Override
