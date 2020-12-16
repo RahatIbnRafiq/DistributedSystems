1538b2e999d0f318f79946f6088f5cde26348cfe
RahatIbnRafiq <rahatibnrafiq@gmail.com>
2020-12-15 19:29:38 -0500

    create service registry node method

--- DistributedSearchSystem/src/main/java/cluster/management/ServiceRegistry.java
+++ DistributedSearchSystem/src/main/java/cluster/management/ServiceRegistry.java
@@ -1,5 +1,8 @@
 package cluster.management;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
 
 public class ServiceRegistry {
@@ -9,5 +12,18 @@ public class ServiceRegistry {
     public ServiceRegistry(ZooKeeper zooKeeper, String serviceRegistryZnode) {
         this.zooKeeper = zooKeeper;
         this.serviceRegistryZnode = serviceRegistryZnode;
+        createServiceRegistryNode();
+    }
+
+    private void createServiceRegistryNode() {
+        try {
+            if(zooKeeper.exists(this.serviceRegistryZnode, false) == null) {
+                this.zooKeeper.create(this.serviceRegistryZnode,new byte [] {}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            }
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
